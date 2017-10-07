ab545579b1ee1178eb5cd08863e99e4595e5732f
lenamson001 <31654967+lenamson001@users.noreply.github.com>
2017-10-08 01:45:25 +0700

    update

--- css-js/index.js
+++ css-js/index.js
@@ -1,8 +1,19 @@
 $(document).ready( function() {
-    
+    /* Check width on page load*/
     if ( $(window).width() > 767) {
-     $('a').removeAttr('data-toggle');
+     $('.nav-link.dropdown-toggle').removeAttr('data-toggle');
+    }
+    else {
+    	
     }
  });
 
+$(window).resize(function() {
+    /*If browser resized, check width again */
+    if ($(window).width() > 767) {
+     $('.nav-link.dropdown-toggle').removeAttr('data-toggle');
+    }
+    else {$('.nav-link.dropdown-toggle').attr('data-toggle','dropdown');}
+ });
  
+
